6619451e36c25f20fe101400e6fabc476821fe0e
Sandesh SIngh <sandeshsingh201106@gmail.com>
2026-02-27 20:32:13 +0530

    connected database

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config();
+connectDB();
+dotenv.config();
+connectDB();
+// import express from "express"
+// const app=express();
+// (async()=>{
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     app.on("error",(error)=>{
+//       console.log("error",error);
+//       throw error
+//     })
+//     app.listen(process.env.PORT,()=>{
+//       console.log(`app is listening at port:http://localhost:${process.env.PORT}`);
+//     })
+//   } catch (error) {
+//     console.error(error);
+//     throw error
+//   }
+// })()
