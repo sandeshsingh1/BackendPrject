f2a447a049830cd9a8ea22cfc37cc7160922ded3
Sandesh SIngh <sandeshsingh201106@gmail.com>
2026-03-02 00:53:47 +0530

    uploaded photo on multer

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,4 +57,33 @@ userSchema.pre("save",async function(next){
 userSchema.methods.isPasswordCorrect=async function(password){
    return await bcrypt.compare(password,this.password)
 }
+userSchema.methods.generateAccessToken= function(){
+ return jwt.sign(
+   {
+     _id: this.id,
+     email: this.email,
+     username: this.username,
+     fullname: this.fullname
+   },
+   process.env.ACCESS_TOKEN_SECRET,
+   {
+     expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+   }
+ )
+}
+userSchema.methods.generateRefreshToken= function(){
+ return jwt.sign(
+   {
+     _id: this.id,
+     email: this.email,
+     username: this.username,
+     fullname: this.fullname
+   },
+   process.env.Refresh_TOKEN_SECRET,
+   {
+     expiresIn: process.env.Refresh_TOKEN_EXPIRY
+   }
+ )
+}
+userSchema.methods.generateRefreshToken=function(){}
 export const  User=mongoose.model("User",userSchema);
