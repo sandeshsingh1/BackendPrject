8735de9235f315611e98091e0426ecf91a232875
Sandesh SIngh <sandeshsingh201106@gmail.com>
2026-03-03 03:22:27 +0530

    Added user and sent response from server

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";//see cookie parser
 const app=express();
 app.use(cors(
 {
-  origin:process.env.CORS_ORIGIN// kaun se origin  wali request allowed honge 
+  origin:process.env.CORS_ORIGIN // kaun se origin  wali request allowed honge 
   ,
 }
 ))
@@ -12,4 +12,8 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser());
+// import routes
+import router from "./routes/user.route.js";
+// isme app.get nhai use kr skate kyuni ab separate ho chuka hai controller.. isliye middleware use krte hain.
+app.use("/users",router)// ab control router ke pas chla gaya wo file execute hogi......  
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -9,9 +9,12 @@
 //   }
 // }
 //using promise method 2
-const asynchandler=(fn)=>{
-  (req,res,next)=>{
-    Promise.resolve(fn(req,res,next)).
-    catch((err)=>next(err))
-  }
-}
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise
+      .resolve(requestHandler(req, res, next))
+      .catch((err) => next(err));
+  };
+};
+
+export default asyncHandler;
